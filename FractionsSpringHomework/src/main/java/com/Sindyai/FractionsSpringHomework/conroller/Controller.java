e90b7a2ca1e66201333cea77d4ce893f790b17d1
Sinyak31 <Sindyai31@gmail.com>
2024-01-27 22:00:40 +0400

    changing the methods of action with fractions

--- FractionsSpringHomework/src/main/java/com/Sindyai/FractionsSpringHomework/conroller/Controller.java
+++ FractionsSpringHomework/src/main/java/com/Sindyai/FractionsSpringHomework/conroller/Controller.java
@@ -3,45 +3,49 @@ package com.Sindyai.FractionsSpringHomework.conroller;
 
 import com.Sindyai.FractionsSpringHomework.utill.ActionsWithFractions;
 import jakarta.websocket.server.PathParam;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.server.WebServerException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fraction")
 public class Controller {
-    
+
 
     @GetMapping("/correct")
     public boolean correctFraction(@PathParam("nom") Integer nom,@PathParam("den") Integer den){
         return ActionsWithFractions.correctFraction(nom, den);
 
     }
-    @GetMapping("/fraction-reduction")
-    public String fractionReduction(@PathParam("nom") Integer nom,@PathParam("den") Integer den,@PathParam("den") Integer dev){
-        return ActionsWithFractions.reduction(nom,den,dev);
+    @GetMapping("/reduction")
+    public String fractionReduction(@PathParam("nom") Integer nom,@PathParam("den") Integer den){
+        return ActionsWithFractions.reduction(nom,den);
     }
 
-    @GetMapping("/fraction-addition")
+    @GetMapping("/addition")
     public String fractionAddition(@PathParam("nom1") Integer nom1,@PathParam("den1") Integer den1,
                                    @PathParam("nom2") Integer nom2,@PathParam("den1") Integer den2){
         return ActionsWithFractions.addition(nom1,den1,nom2,den2);
     }
 
-    @GetMapping("/subtraction-addition")
+    @GetMapping("/subtraction")
     public String fractionSubtraction(@PathParam("nom1") Integer nom1,@PathParam("den1") Integer den1,
                                    @PathParam("nom2") Integer nom2,@PathParam("den1") Integer den2){
-        return ActionsWithFractions.addition(nom1,den1,nom2,den2);
+        return ActionsWithFractions.subtraction(nom1,den1,nom2,den2);
     }
 
-    @GetMapping("/division-addition")
+    @GetMapping("/division")
     public String fractionDivision(@PathParam("nom1") Integer nom1,@PathParam("den1") Integer den1,
                                       @PathParam("nom2") Integer nom2,@PathParam("den1") Integer den2){
-        return ActionsWithFractions.addition(nom1,den1,nom2,den2);
+        return ActionsWithFractions.division(nom1,den1,nom2,den2);
     }
 
-    @GetMapping("/multiplication-addition")
+    @GetMapping("/multiplication")
     public String fractionMultiplication(@PathParam("nom1") Integer nom1,@PathParam("den1") Integer den1,
                                       @PathParam("nom2") Integer nom2,@PathParam("den1") Integer den2){
-        return ActionsWithFractions.addition(nom1,den1,nom2,den2);
+        return ActionsWithFractions.multiplication(nom1,den1,nom2,den2);
     }
 
 }
--- FractionsSpringHomework/src/main/java/com/Sindyai/FractionsSpringHomework/utill/ActionsWithFractions.java
+++ FractionsSpringHomework/src/main/java/com/Sindyai/FractionsSpringHomework/utill/ActionsWithFractions.java
@@ -1,41 +1,65 @@
 package com.Sindyai.FractionsSpringHomework.utill;
 
 public class ActionsWithFractions {
-    public static boolean correctFraction(int nominator, int denominator){
-        return nominator<denominator;
+    public static boolean correctFraction(int nominator, int denominator) {
+        return nominator < denominator;
     }
 
-    public static String reduction(int nominator,int denominator, int divider){
-if(nominator%divider==0 && denominator%divider==0){
-    return nominator+"/"+denominator+" после сокращения на "+divider+"\n"+
-            nominator/divider+"/"+denominator/divider;
+    public static String reduction(int nominator, int denominator) {
+        int[]res =getShortenedFraction(nominator,denominator);
+        return nominator+"/"+denominator+" после сокращения = "+res[0]+"/"+res[1];
+    }
 
+    public static String addition(int nom1, int den1, int nom2, int den2) {
+        int nom1Result=nom1*den2;
+        int denResult = den1*den2;
+        int nom2Result=nom2*den1;
+        int[]res =getShortenedFraction(nom1Result+nom2Result,denResult);
+        return nom1 + "/" + den1 + " + " + nom2 + "/" + den2 + " = " + res[0]+"/"+res[1];
+    }
+
+
+    public static String subtraction(int nom1, int den1, int nom2, int den2) {
+        int nom1Result=nom1*den2;
+        int denResult = den1*den2;
+        int nom2Result=nom2*den1;
+        int[]res =getShortenedFraction(nom1Result-nom2Result,denResult);
+        return nom1 + "/" + den1 + " - " + nom2 + "/" + den2 + " = " + res[0]+"/"+res[1];
+    }
+
+    public static String division(int nom1, int den1, int nom2, int den2) {
+        int[]res =getShortenedFraction(nom1*den2,den1*nom2);
+        return nom1 + "/" + den1 + " / " + nom2 + "/" + den2 + " = " + res[0]+"/"+res[1];
+    }
+
+    /*
+    Метод умножения
+    В параметры передаем  2 дроби (nom1/den1 и nom2/den2)
+    */
+
+    public static String multiplication(int nom1, int den1, int nom2, int den2) {
+        int[]res =getShortenedFraction(nom1*nom2,den1*den2); //передаем результат умножения дробей и получаем сокращенную дробь
+        return nom1 + "/" + den1 + " * " + nom2 + "/" + den2 + " = " + res[0]+"/"+res[1];
+    }
+
+
+    //Метод сокращения дробей
+private static int[] getShortenedFraction(int nom,int den){
+        int[]result = new int[2];
+        int max = 1;
+    if (den % nom == 0) {
+        result[0]= 1;
+        result[1] = den/ nom;
+    }
+       else{
+        for (int i = 2; i < den; i++) {
+            if(nom%i ==0 && den%i==0)
+                max=i;
+        }
+        result[0]= nom/max;
+        result[1] = den/ max;
+    }
+       return result;
 }
-    else {
-        return "Дробь невозможно сократить";
-}
-    }
-
-    public static String addition(int nom1,int den1,int nom2,int den2){
-double result=((double) nom1/den2)+((double)nom2/den2);
-        return String.valueOf(nom1+"/"+den1+" + "+nom2+"/"+den2+" = "+result);
-    }
-
-
-    public static String subtraction(int nom1,int den1,int nom2,int den2) {
-        double result = ((double) nom1 / den2) - ((double) nom2 / den2);
-        return String.valueOf(nom1 + "/" + den1 + " + " + nom2 + "/" + den2 + " = " + result);
-    }
-
-    public static String division(int nom1,int den1,int nom2,int den2) {
-        double result = ((double) nom1 / den2) / ((double) nom2 / den2);
-        return String.valueOf(nom1 + "/" + den1 + " + " + nom2 + "/" + den2 + " = " + result);
-    }
-
-    public static String multiplication(int nom1,int den1,int nom2,int den2) {
-        double result = ((double) nom1 / den2) * ((double) nom2 / den2);
-        return String.valueOf(nom1 + "/" + den1 + " + " + nom2 + "/" + den2 + " = " + result);
-    }
-
 
 }
